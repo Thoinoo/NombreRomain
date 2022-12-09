5e8c72b350bbdc1237476364067bffc5a942e167
thoin <thoino@outlook.fr>
2022-12-09 12:16:07 +0100

    modification logique fonction convertir, test 1 à 30 passé

--- NombreRomain/Convertisseur.cs
+++ NombreRomain/Convertisseur.cs
@@ -6,26 +6,13 @@ namespace NombreRomain
     {
         public static object convertir(int chiffre)
         {
-            
-            return Unite.getSymbole(chiffre);
+            string temp = "";
+            temp += Dizaine.getSymbole(chiffre / 10);
+            temp += Unite.getSymbole(chiffre % 10);
 
-            return chiffre switch
-            {
-                <= 3 => new string('I', chiffre),
-                4 => "IV",
-                <= 8 => "V" + new string('I', chiffre - 5),
-                9=> "IX",
-                <= 13 => "X" + new string('I', chiffre - 10),
-                14 => "XIV",
-                <= 18 => "XV" + new string('I', chiffre - 15),
-                19 => "XIX",
-                <= 23 => "XX" + new string('I', chiffre - 20),
-                24 => "XXIV",
-                <= 28 => "XXV" + new string('I', chiffre - 25),
-                29 => "XXIX",
-                30 => "XXX",
-                _ => new NotImplementedException()
-            };
+            return temp;
+            
+
         }
 
     }
---
+++ NombreRomain/Dizaine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NombreRomain
+{
+    internal static class Dizaine
+    {
+        private static ChiffreRomain zero = new ChiffreRomain(0,"");
+        private static ChiffreRomain un = new ChiffreRomain(1,"X");
+        private static ChiffreRomain deux = new ChiffreRomain(2,"XX" );
+        private static ChiffreRomain trois = new ChiffreRomain(3, "XXX");
+        private static ChiffreRomain quatre = new ChiffreRomain(4, "XL");
+        private static ChiffreRomain cinq = new ChiffreRomain(5, "L");
+        private static ChiffreRomain six = new ChiffreRomain(6, "LX");
+        private static ChiffreRomain sept = new ChiffreRomain(7, "LXX");
+        private static ChiffreRomain huit = new ChiffreRomain(8, "LXXX");
+        private static ChiffreRomain neuf = new ChiffreRomain(9, "XC");
+
+        private static ChiffreRomain[] listChiffre = new[] { zero, un, deux, trois, quatre, cinq, six, sept, huit, neuf };
+
+        public static string getSymbole(int n)
+        {
+            foreach (var chiffre in listChiffre)
+            {
+                if (chiffre.getValeur == n)
+                {
+                    return chiffre.getSymbole;
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
